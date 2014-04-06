09dda8e29196d17b04e8fb7649d5ea6034d7eef3
antoine <duvillierantoine@gmail.com>
2014-04-06 23:42:07 +0200

    set sails

--- app.js
+++ app.js
@@ -1,15 +1,2 @@
 // Start sails and pass it command line arguments
-/*require('sails').lift(require('optimist').argv);
-*/
-// web.js
-var express = require("express");
-var app = express();
-
-app.get('/', function(req, res) {
-  res.send('Hello World! 5925f8c6');
-});
-
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+require('sails').lift(require('optimist').argv);
