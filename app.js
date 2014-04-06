a349bcfb2f889153613bc6454ec1853f213b19a9
antoine <duvillierantoine@gmail.com>
2014-04-06 21:05:50 +0200

    try launch server

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 // Start sails and pass it command line arguments
-require('sails').lift(require('optimist').argv);
+/*require('sails').lift(require('optimist').argv);
+*/
+// web.js
+var express = require("express");
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send('Hello World! 5925f8c6');
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
